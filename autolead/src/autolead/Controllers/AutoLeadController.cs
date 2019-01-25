7bae563c0fda3bf8b88451512dc9893efe1b31d6
rmadineedi2331 <47020961+rmadineedi2331@users.noreply.github.com>
2019-01-24 21:27:41 -0800

    Add files via upload

---
+++ autolead/src/autolead/Controllers/AutoLeadController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace autolead.Controllers
+{
+  
+    public class AutoLeadController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        [Route("API/AutoLead/GetAllAutoLeadList")]
+        public string GetAllAutoLeadList()
+        {
+            return JsonReadHelper.GetAllAutoLeads();
+        }
+
+        [HttpGet]
+        [Route("API/AutoLead/GetAutoLeadById/{Id}")]
+        public string GetAutoLeadById(int Id)
+        {
+            return JsonReadHelper.GetAutoLeadById(Id);
+        }
+
+        [HttpGet]
+        [Route("API/AutoLead/GetAutoLeadByState/{State}")]
+        public string GetAutoLeadByState(string State)
+        {
+            return JsonReadHelper.GetAutoLeadByState(State);
+        }
+
+        [HttpGet]
+        [Route("API/AutoLead/GetAutoLeadByMake/{Make}")]
+        public string GetAutoLeadByMake(string Make)
+        {
+            return JsonReadHelper.GetAutoLeadByMake(Make);
+        }
+
+        [HttpGet]
+        [Route("API/AutoLead/GetAutoLeadByinurer/{Insurare}")]
+        public string GetAutoLeadByinurer(string Insurare)
+        {
+            return JsonReadHelper.GetAutoLeadByFormerinsurer(Insurare);
+        }
+
+             
+
+      
+
+    }
+}
---
+++ autolead/src/autolead/JsonReadHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace autolead
+{
+    public static class JsonReadHelper
+    {
+
+        public static string GetAllAutoLeads()
+        {
+            FileStream filestream = new FileStream("auto.leads.json", FileMode.Open);
+            using (StreamReader r = new StreamReader(filestream))
+            {
+                string json = r.ReadToEnd();        
+                return json;
+            }
+        }
+
+        public static string GetAutoLeadById(int Id)
+        {
+            FileStream filestream = new FileStream("auto.leads.json", FileMode.Open);
+            using (StreamReader r = new StreamReader(filestream))
+            {
+                string json = r.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+                var byid = from idval in (IEnumerable<dynamic>)array
+                           where idval.id == Id 
+                           select new
+                           {
+                               idval
+                           };
+                return JsonConvert.SerializeObject(byid);
+            }
+        }
+
+        public static string GetAutoLeadByState(string State)
+        {
+            FileStream filestream = new FileStream("auto.leads.json", FileMode.Open);
+            using (StreamReader r = new StreamReader(filestream))
+            {
+                string json = r.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+                var byState = from state in (IEnumerable<dynamic>)array
+                           where state.consumer.state == State
+                           select new
+                           {
+                               state
+                           };
+                return JsonConvert.SerializeObject(byState);
+            }
+        }
+
+        public static string GetAutoLeadByMake(string Make)
+        {
+            FileStream filestream = new FileStream("auto.leads.json", FileMode.Open);
+            using (StreamReader r = new StreamReader(filestream))
+            {
+                string json = r.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+                var byMake = from make in (IEnumerable<dynamic>)array
+                           from vehicles in (IEnumerable<dynamic>)make.vehicle
+                           where vehicles.make == Make
+                           select new
+                           {
+                               make
+                           };
+                return JsonConvert.SerializeObject(byMake);
+            }
+        }
+
+        public static string GetAutoLeadByFormerinsurer(string insurar)
+        {
+            FileStream filestream = new FileStream("auto.leads.json", FileMode.Open);
+            using (StreamReader r = new StreamReader(filestream))
+            {
+                string json = r.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+                var byinsurance = from insurance in (IEnumerable<dynamic>)array
+                           where insurance.coverage.former_insurer == insurar
+                           select new
+                           {
+                               insurance
+                           };
+                return JsonConvert.SerializeObject(byinsurance);
+            }
+        }
+
+
+    }
+}
